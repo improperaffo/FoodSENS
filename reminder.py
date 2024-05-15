4b114cdbdb4aeb6e409e6d65c33c87023a5b20c2
Raphael <rafo.canna1808@gmail.com>
2024-05-15 22:23:15 +0200

    fix reminder

--- reminder.py
+++ reminder.py
@@ -1,10 +1,10 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 # Load environment variables
-load_dotenv("config.env")
+#load_dotenv("config.env")
 slack_token = os.getenv("SLACK_TOKEN")
 
 # Start Slack session
